de70aec64fd8f470c3751e7f9b0abb74d2548bfd
Normandy <rn.sgnaolin.com>
2024-04-22 14:00:14 -0300

    Fixed automated test to new structure

--- src/test/java/world/gta/saaa/aircraft/controller/AircraftControllerTest.java
+++ src/test/java/world/gta/saaa/aircraft/controller/AircraftControllerTest.java
@@ -4,6 +4,7 @@ import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.Arrays;
 
@@ -65,17 +66,18 @@ class AircraftControllerTest {
 
     }
 
-/*     @Test
+    @Test
     @DisplayName("Should return HTTP Status 201")
     @SuppressWarnings("null")
     void testRegisterAircraft2() throws Exception {
 
         var data = new AircraftDTO("Brand", "Model", "N156AC", "N156AC", Classification.GOVERNMENT, 1l);
-        var personData = new Person(1l, "John Doe", "John Doe", 7518l);
+        var personData = new Person(1l, "John Doe", "John Doe", 7518l, null);
+
         var aircraftData = new Aircraft(1l, "Brand", "Model", "N156AC", "N156AC", false, true, Classification.GOVERNMENT, personData);
 
         var expectedJson = aircraftListingJson.write(
-            new AircraftListingDTO("Brand", "Model", "GOVERNMENT", "N156AC", "N156AC", "John Doe", true)
+            new AircraftListingDTO(1l, "Brand", "Model", "GOVERNMENT", "N156AC", "N156AC", "John Doe", true)
         ).getJson();
 
         when(aircraftService.dataToObject(data)).thenReturn(aircraftData);
@@ -92,55 +94,5 @@ class AircraftControllerTest {
 
             
     }
-
-    @Test
-    @DisplayName("Should return invalid tail numbers correctly")
-    void testInvalidTailNumbers() throws Exception {
-
-        Person person = new Person(1l, "Government Company", "John Doe", 123456l);
-
-        List<Aircraft> aircrafts = Arrays.asList(
-            new Aircraft(1l, "Brand1", "Model1", "AIR 1", "AIR 1", false, true, Classification.GOVERNMENT, person),
-            new Aircraft(2l, "Brand2", "Model2", "N156AC", "N156AC", false, true, Classification.GOVERNMENT, person),
-            new Aircraft(3l, "Brand3", "Model3", "N12345", "N12345", false, true, Classification.GOVERNMENT, person),
-            new Aircraft(4l, "Brand4", "Model4", "N15ABC", "N15ABC", false, true, Classification.GOVERNMENT, person),
-            new Aircraft(5l, "Brand5", "Model5", "N156IB", "N156IB", false, true, Classification.GOVERNMENT, person),
-            new Aircraft(6l, "Brand6", "Model6", "N156OC", "156OC", false, true, Classification.GOVERNMENT, person)
-        );
-
-        List<Aircraft> invalidAircrafts = Arrays.asList(
-            new Aircraft(1l, "Brand1", "Model1", "AIR 1", "AIR 1", false, true, Classification.GOVERNMENT, person),
-            new Aircraft(4l, "Brand4", "Model4", "N15ABC", "N15ABC", false, true, Classification.GOVERNMENT, person),
-            new Aircraft(5l, "Brand5", "Model5", "N156IB", "N156IB", false, true, Classification.GOVERNMENT, person),
-            new Aircraft(6l, "Brand6", "Model6", "N156OC", "N156OC", false, true, Classification.GOVERNMENT, person)
-        );
-
-        List<AircraftListingDTO> dataOfInvalid = invalidAircrafts.stream().map(AircraftListingDTO::new).collect(Collectors.toList());
-
-        when(aircraftService.getAircraftRepository()).thenReturn(aircraftRepository);
-        when(aircraftRepository.findAllIrregularTailNumbers()).thenReturn(regexForInvalidTailNumbers(aircrafts));
-
-        var expectedJson = listOfAircraftListingJson.write(dataOfInvalid).getJson();
-        var response = mvc.perform(MockMvcRequestBuilders.get("/aircrafts/invalid"))
-            .andReturn()
-            .getResponse();
-
-        Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
-        Assertions.assertThat(response.getContentAsString()).contains(expectedJson);
-
-    }
-
-    List<Aircraft> regexForInvalidTailNumbers(List<Aircraft> aircrafts) {
-
-        List<Aircraft> results = new ArrayList<>();
-
-        for (Aircraft a : aircrafts) {
-            if (!a.validTailNumber()) {
-                results.add(a);
-            }
-        }
-
-        return results;
-    } */
 
 }
